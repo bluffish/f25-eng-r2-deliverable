977cf3d6cac27ac561fcdb5f12aaefa5910368b3
Bowen Yang <bowenymail@gmail.com>
2026-02-09 19:28:19 -0500

    tasks 1 and 2

---
+++ app/species/edit-species-dialog.tsx
@@ -0,0 +1,297 @@
+"use client";
+
+// stuff taken directly from add-species-dialog.tsx
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
+import { createBrowserSupabaseClient } from "@/lib/client-utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState, type BaseSyntheticEvent } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// We use zod (z) to define a schema for the "Add species" form.
+// zod handles validation of the input values with methods like .string(), .nullable(). It also processes the form inputs with .transform() before the inputs are sent to the database.
+
+// Define kingdom enum for use in Zod schema and displaying dropdown options in the form
+const kingdoms = z.enum(["Animalia", "Plantae", "Fungi", "Protista", "Archaea", "Bacteria"]);
+
+// Use Zod to define the shape + requirements of a Species entry; used in form validation
+const speciesSchema = z.object({
+  scientific_name: z
+    .string()
+    .trim()
+    .min(1)
+    .transform((val) => val?.trim()),
+  common_name: z
+    .string()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+  kingdom: kingdoms,
+  total_population: z.number().int().positive().min(1).nullable(),
+  image: z
+    .string()
+    .url()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+  description: z
+    .string()
+    .nullable()
+    // Transform empty string or only whitespace input to null before form submission, and trim whitespace otherwise
+    .transform((val) => (!val || val.trim() === "" ? null : val.trim())),
+});
+
+type FormData = z.infer<typeof speciesSchema>;
+
+// get values from database as default
+function getDefaultValues(species): Partial<FormData> {
+  const kingdom = species.kingdom
+  return {
+    scientific_name: species.scientific_name ?? "",
+    common_name: species.common_name ?? null,
+    kingdom,
+    total_population:
+      species.total_population !== null && species.total_population !== undefined
+        ? Number(species.total_population)
+        : null,
+    image: species.image ?? null,
+    description: species.description ?? null,
+  };
+}
+
+export default function EditSpeciesDialog({ species, userId }: { species: Species, userId: string }) {
+  const router = useRouter();
+  const [open, setOpen] = useState<boolean>(false);
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(speciesSchema),
+    defaultValues: getDefaultValues(species),
+    mode: "onChange",
+  });
+
+  // console.log(species.author);
+  // console.log(userId);
+  if (species.author !== userId) {
+    // return <div>You are not authorized to edit this species.</div>;
+    return;
+  }
+  // console.log(species.author);
+
+  // Reset form when dialog opens so it shows current species data
+  const handleOpenChange = (next: boolean) => {
+    if (next) form.reset(getDefaultValues(species));
+    setOpen(next);
+  };
+
+  const onSubmit = async (input: FormData) => {
+    const supabase = createBrowserSupabaseClient();
+    const { error } = await supabase
+      .from("species")
+      .update({
+        common_name: input.common_name,
+        description: input.description,
+        kingdom: input.kingdom,
+        scientific_name: input.scientific_name,
+        total_population: input.total_population,
+        image: input.image,
+      })
+      .eq("id", species.id);
+
+    if (error) {
+      return toast({
+        title: "Something went wrong.",
+        description: error.message,
+        variant: "destructive",
+      });
+    }
+
+    form.reset(getDefaultValues({ ...species, ...input }));
+    setOpen(false);
+    router.refresh();
+
+    return toast({
+      title: "CONGRATS!!!",
+      description: `Successfully updated ${species.scientific_name}.`,
+    });
+  };
+
+  const onDelete = async () => {
+    const supabase = createBrowserSupabaseClient();
+    const { error } = await supabase
+      .from("species")
+      .delete()
+      .eq("id", species.id);
+
+    setOpen(false);
+    router.refresh();
+
+    return toast({
+      title: "Bupsti! (who gets the reference)",
+      description: `Successfully deleted ${species.scientific_name}.`,
+    });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleOpenChange}>
+      <DialogTrigger asChild>
+        <Button variant="ghost" className="min-w-0 flex-1">Edit</Button>
+
+      </DialogTrigger>
+      <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>Edit Species</DialogTitle>
+          <DialogDescription>
+            Cute little description for updating the species information should go here!
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={(e: BaseSyntheticEvent) => void form.handleSubmit(onSubmit)(e)}>
+            <div className="grid w-full items-center gap-4">
+              <FormField
+                control={form.control}
+                name="scientific_name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Scientific Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Cavia porcellus" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="common_name"
+                render={({ field }) => {
+                  const { value, ...rest } = field;
+                  return (
+                    <FormItem>
+                      <FormLabel>Common Name</FormLabel>
+                      <FormControl>
+                        <Input value={value ?? ""} placeholder="Guinea pig" {...rest} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="kingdom"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Kingdom</FormLabel>
+                    <Select onValueChange={(value) => field.onChange(kingdoms.parse(value))} value={field.value}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a kingdom" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        <SelectGroup>
+                          {kingdoms.options.map((k, index) => (
+                            <SelectItem key={index} value={k}>
+                              {k}
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="total_population"
+                render={({ field }) => {
+                  const { value, ...rest } = field;
+                  return (
+                    <FormItem>
+                      <FormLabel>Total population</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          value={value ?? ""}
+                          placeholder="300000"
+                          {...rest}
+                          onChange={(event) => field.onChange(event.target.value === "" ? null : +event.target.value)}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => {
+                  const { value, ...rest } = field;
+                  return (
+                    <FormItem>
+                      <FormLabel>Image URL</FormLabel>
+                      <FormControl>
+                        <Input
+                          value={value ?? ""}
+                          placeholder="https://..."
+                          {...rest}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => {
+                  const { value, ...rest } = field;
+                  return (
+                    <FormItem>
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea value={value ?? ""} placeholder="Description..." {...rest} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+              <div className="flex">
+                <Button type="submit" className="ml-1 mr-1 flex-auto">
+                  Save
+                </Button>
+                <Button type="button" variant="destructive" className="ml-1 mr-1 flex-auto" onClick={onDelete}>
+                  Delete
+                </Button>
+                <DialogClose asChild>
+                  <Button type="button" className="ml-1 mr-1 flex-auto" variant="secondary">
+                    Cancel
+                  </Button>
+                </DialogClose>
+              </div>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -10,12 +10,26 @@ on the client-side to correctly match component state and props should the order
 React server components don't track state between rerenders, so leaving the uniquely identified components (e.g. SpeciesCard)
 can cause errors with matching props and state in child components if the list order changes.
 */
+import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import type { Database } from "@/lib/schema";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import Image from "next/image";
+import { useState } from "react";
+import EditSpeciesDialog from "./edit-species-dialog";
+
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
-export default function SpeciesCard({ species }: { species: Species }) {
+export default function SpeciesCard({ species, userId }: { species: Species, userId: string }) {
+  const [open, setOpen] = useState<boolean>(false);
+  
   return (
     <div className="m-4 w-72 min-w-72 flex-none rounded border-2 p-3 shadow">
       {species.image && (
@@ -26,8 +40,61 @@ export default function SpeciesCard({ species }: { species: Species }) {
       <h3 className="mt-3 text-2xl font-semibold">{species.scientific_name}</h3>
       <h4 className="text-lg font-light italic">{species.common_name}</h4>
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
-      {/* Replace the button with the detailed view dialog. */}
-      <Button className="mt-3 w-full">Learn More</Button>
+      
+      <div className="mt-3 flex flex-nowrap gap-2">
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button variant="ghost"className="min-w-0 flex-1">Learn More</Button>
+          </DialogTrigger>
+          <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+            <DialogHeader>
+              <DialogTitle>Species information</DialogTitle>
+              <DialogDescription>
+                Here&apos;s a great place to learn more about this species! Stay in school, kids.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="mt-4 space-y-6">
+              <div className="mx-auto w-full max-w-[420px] overflow-hidden rounded-s border">
+                <div className="divide-y">
+                  <div className="grid grid-cols-[160px_1fr] gap-3 px-4 py-3 text-sm">
+                    <div className="font-medium text-muted-foreground">Common name</div>
+                    <div>{species.common_name ?? "Unknown"}</div>
+                  </div>
+
+                  <div className="grid grid-cols-[160px_1fr] gap-3 px-4 py-3 text-sm">
+                    <div className="font-medium text-muted-foreground">Scientific name</div>
+                    <div className="italic">{species.scientific_name ?? "Unknown"}</div>
+                  </div>
+
+                  <div className="grid grid-cols-[160px_1fr] gap-3 px-4 py-3 text-sm">
+                    <div className="font-medium text-muted-foreground">Kingdom</div>
+                    <div>{species.kingdom ?? "Unknown"}</div>
+                  </div>
+
+                  <div className="grid grid-cols-[160px_1fr] gap-3 px-4 py-3 text-sm">
+                    <div className="font-medium text-muted-foreground">Total population</div>
+                    <div>
+                      {species.total_population === null ||
+                      species.total_population === undefined
+                        ? "Unknown"
+                        : Number(species.total_population).toLocaleString()}
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-[160px_1fr] gap-3 px-4 py-3 text-sm">
+                    <div className="font-medium text-muted-foreground">Description</div>
+                    <div className="leading-relaxed">
+                      {species.description ?? "Unknown"}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </DialogContent>
+        </Dialog>
+        <EditSpeciesDialog species={species} userId={userId} />
+      </div>
+      
     </div>
   );
 }
